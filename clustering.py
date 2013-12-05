c95b452a98b0109d937cd212692a1e3a47d748a0
Sean Adler <sadler13@cmc.edu>
2013-12-05 12:16:40 -0800

    working on cv

--- clustering.py
+++ clustering.py
@@ -1,14 +1,31 @@
-import sklearn.cluster
+from sklearn.cluster import KMeans
+from sklearn.cross_validation import KFold
 import numpy as np
 
-csv = '/Users/SDA/Fall 2013/CS158/semeion.csv'
+csv = '/Users/SDA/Fall 2013/CS158/kmeanscluster/semeion.csv'
 
 dataset = np.genfromtxt(open(csv), delimiter=',')
 
 #  Use 10 clusters.  (k=10)
-kmeans = sklearn.cluster.KMeans(10)
+kmeans = KMeans(10)
 
 target = np.array([x[-1] for x in dataset])
 train = np.array([x[:-1] for x in dataset])
 
 kmeans.fit(train)
+
+cv = KFold(len(train), n_folds=4)
+
+
+#  Train all exs at once
+#kmeans = KMeans(10)
+#kmeans.fit([train[traincv] for 
+
+for traincv, testcv in cv:
+    print len(traincv)
+    print traincv
+    print testcv
+    #kmeans = KMeans(10).fit(train[traincv])
+
+     #   print kmeans.predict(train[ex])
+      #  print target[traincv]
