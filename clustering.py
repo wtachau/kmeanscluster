8f934aa7ae2594621801ef8f9d0874dc874f43df
Will Tachau <wtachau@gmail.com>
2013-12-05 12:17:31 -0800

    pdb

--- clustering.py
+++ clustering.py
@@ -1,9 +1,12 @@
 import sklearn.cluster
+from sklearn import cross_validation
 import numpy as np
+import pdb
 
-csv = '/Users/SDA/Fall 2013/CS158/semeion.csv'
+csv = 'semeion.csv'
 
 dataset = np.genfromtxt(open(csv), delimiter=',')
+
 
 #  Use 10 clusters.  (k=10)
 kmeans = sklearn.cluster.KMeans(10)
@@ -11,4 +14,19 @@ kmeans = sklearn.cluster.KMeans(10)
 target = np.array([x[-1] for x in dataset])
 train = np.array([x[:-1] for x in dataset])
 
-kmeans.fit(train)
+# cross validation
+cv = cross_validation.KFold(len(train), n_folds=5, indices=False)
+
+num_correct = 0.0
+num_total = 0.0
+#iterate through the training and test cross validation segments
+for traincv, testcv in cv:
+    # FIT THE LEARNER YOU ARE TESTING
+    probas = kmeans.fit(train[traincv], target[traincv])
+    pdb.set_trace()
+    for index, x in enumerate(probas):
+    	
+        highest_prob = np.where(x==max(x))[0][0]
+        if target[testcv][index] == highest_prob:
+            num_correct+=1
+        num_total+=1
