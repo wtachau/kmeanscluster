ab794724ee8a8b90d856df6bb0627761e7ca2929
Will Tachau <wtachau@gmail.com>
2013-12-10 12:50:39 -0800

    generated random subsets of incorrect, correct classifications

--- awesome_cluster.py
+++ awesome_cluster.py
@@ -68,8 +68,55 @@ def showRepDigits(data):
 
     print cluster_indices
 
-    #now write each to file
+
+# For each cluster, generate random subset of both correctly and 
+# incorrectly classified data
+def generateRandomSubset(data):
+    # first train on all the data
+    kmeans = KMeans(init='k-means++', n_clusters=10, n_init=10)
+    train = [row[:-1] for row in data.examples]
+    scaled = scale(train)
+    kmeans.fit(scaled)
     
+    cluster_label_dict = clusterLabelDict(kmeans, data)
+
+    def predict(ex):
+        scaled = scale(ex[:-1])
+        cluster = kmeans.predict(scaled)[0]   
+        return cluster_label_dict[cluster]
+
+    classifications = []
+    for i in range(10):
+        correct_class = []
+        incorrect_class = []
+        for ind, ex in enumerate(data.examples):
+            pred = predict(ex)
+            actual = ex[-1]
+            if (pred == i):
+                if (pred == actual):
+                    correct_class.append(ind)
+                else:
+                    incorrect_class.append(ind)
+        classifications.append({'num':i, 'correct':correct_class, 'incorrect':incorrect_class})
+    
+    # now generate random subset
+    import random
+    for num in classifications:
+        num_subset = 5
+        # for correct
+        if (len(num['correct']) >= 5):
+            num['correct_subset'] = random.sample(num['correct'], num_subset)
+        else:
+            num['correct_subset'] = num['correct']
+        # and for incorrect
+        if (len(num['incorrect']) >= 5):
+            num['incorrect_subset'] = random.sample(num['incorrect'], num_subset)
+        else:
+            num['incorrect_subset'] = num['incorrect']
+
+    # at this point classifications has everything -> can print or write to file
+        
+
 
 
 def clusterLabelDict(kmeans, data):
@@ -95,7 +142,9 @@ if __name__ == '__main__':
     #results = cross_validation(kmeansLearner, data)
     #print "Kmeans accuracy: ", results
 
-    showRepDigits(data)
+    #showRepDigits(data)
+
+    generateRandomSubset(data)
 
 
 
